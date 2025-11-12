091e9105dbf08e503caad4eef96d68bf7c43b7d0
Md Amjad hussen <mdamjadhussen1@gmail.com>
2025-11-12 20:22:22 +0600

    my service page designed

---
+++ src/Components/MyService/MyService.jsx
@@ -0,0 +1,115 @@
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+
+
+const MyService = ({ service }) => {
+
+    const [myService, setMyService] = useState([])
+    console.log(service)
+
+
+    useEffect(() => {
+        setMyService(service || []);
+    }, [service]);
+
+    const handleDelete = (_id) => {
+
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+
+                fetch(`http://localhost:3000/allService/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your service has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+
+                    const remainingService = myService.filter(service => service._id !==_id) 
+                    setMyService(remainingService)
+
+
+            }
+        });
+
+
+    }
+
+
+    return (
+        <div className='w-11/12 mx-auto'>
+
+            <h1 className="text-3xl font-black text-orange-500 mb-5">My All Service: {myService.length}</h1>
+            <div className="overflow-x-auto rounded-xl border border-orange-200 ">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th className="px-4 py-2">
+                                <p>My Service</p>
+                            </th>
+                            <th className="px-4 py-2">Service Info</th>
+                            <th className="px-4 py-2">Provider Name</th>
+                            <th className="px-4 py-2">Price</th>
+                            <th className="px-4 py-2">Action</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            myService.map((card, index) => <tr key={card._id}>
+                                <th>
+                                    {index + 1}
+                                </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle h-12 w-12">
+                                                <img
+                                                    src={card.imageUrl}
+                                                    alt="Service Name" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{card.serviceName}</div>
+                                            <div className="text-sm ">{card.category}</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {card.providerName}
+
+                                </td>
+                                <td> $ {card.price}</td>
+                                <th className='flex gap-3'>
+                                    <button className="btn border-2 border-orange-600 text-orange-600 hover:bg-orange-500 hover:text-white font-bold ">Update</button>
+                                    <button onClick={() => handleDelete(card._id)} className="btn bg-orange-500 hover:bg-orange-600 text-white font-bold ">Delete</button>
+                                </th>
+                            </tr>)
+                        }
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default MyService;
--- src/Pages/MyService/MyServices.jsx
+++ src/Pages/MyService/MyServices.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { use, useEffect, useState } from 'react';
+import MyService from '../../Components/MyService/MyService';
+import { AuthContext } from '../../context/AuthContext';
+
+
 
 const MyServices = () => {
+    const {user} = use(AuthContext)
+    const [service, setService] = useState([])
+    
+    useEffect( () => {
+        if(user?.email) {
+            fetch(`http://localhost:3000/allService?email=${user.email}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setService(data)
+            })
+        }
+
+    }, [user?.email])
+
     return (
-        <div>
-            <h1>this is my service page</h1>
+        <div className='mx-auto py-16'>
+            <div className="text-center mb-10">
+                <h2 className="text-3xl md:text-4xl font-bold ">My Listed <span className='text-orange-600'> Services</span></h2>
+                <p className="text-gray-600 mt-3">Manage all your services here. Edit or remove any service as needed.</p>
+            </div>
+
+            <MyService service ={service}></MyService>
         </div>
     );
 };
